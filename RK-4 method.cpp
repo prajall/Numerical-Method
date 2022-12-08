ea1fcfafa2ce9bb38677ddfca1fa38a7b4aa656c
Prajal Maharjan <91400741+prajall@users.noreply.github.com>
2022-12-08 12:28:03 +0545

    Update RK-4 method.cpp

--- RK-4 method.cpp
+++ RK-4 method.cpp
@@ -13,7 +13,6 @@ int main()
 	float h=0.2;
 	
 	for (i=0;i<2;i++){
-		
 		m1 = f(x,y);
 		m2 = f(x+(h/2), y+(h/2*m1));
 		m3 = f(x+(h/2), y+(h/2*m2));
@@ -24,7 +23,6 @@ int main()
 		y = y + (m*h); 
 		x += h;	
 	}
-	cout<<y;
-	
+	cout<<y;	
 	return 0;	
 }
